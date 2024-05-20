c93e51b793b71e0ca6fe0ad1c5b6e3d42bef7829
Emmanuel301004 <emman30102004@gmail.com>
2024-05-20 13:51:31 +0530

    js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let userScore = 0;
 let computerScore = 0;
 const choices = document.querySelectorAll(".choice");
+const playGame=(userChoice) => {
+    console.log("userChoice = ",userChoice);
+}
 choices.forEach((choice) => {
-    console.log(choice);
-    choice.addEventListener("click", () => {
-console.log("choice clicked ");
+        choice.addEventListener("click", () => {
+const userChoice = choice.getAttribute("id");
+             playGame(userChoice);
     });  
-})
+});
